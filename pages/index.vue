1192f2356a2f6f836f53d7374e9dca2603d40c22
dxlliv <hello@dxlliv.dev>
2023-12-05 03:42:13 +0100

    Update image links

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ useHead({
 
 <template>
   <Section height="100vh">
-    <SectionImage height="100vh" src="https://dxlliv.github.io/ig-planner/user/dxlliv/media/6.jpg" />
+    <SectionImage height="100vh" src="https://dxlliv.github.io/planner/user/instagram/dxlliv/media/6.jpg" />
     <Header absolute />
   </Section>
 
