81af86ea9417136d44036a05c2124f261dee3ed0
dxlliv <hello@dxlliv.dev>
2024-05-20 17:36:07 +0200

    Add cdnURL to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
             ],
         },
         buildAssetsDir: 'assets/',
+        cdnURL: 'https://dxlliv.github.io',
     },
     css: [
         "@fontsource/quicksand",
