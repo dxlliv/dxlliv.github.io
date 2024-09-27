3cc8f8239f7e0772ef4d12681d8a0fd5f8eb80b9
dxlliv <hello@dxlliv.dev>
2024-09-27 04:38:57 +0200

    Update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,66 +1,77 @@
 const path = require('path');
 
 export default defineNuxtConfig({
-    ssr: true,
-    app: {
-        head: {
-            htmlAttrs: {
-                translate: 'no',
-                class: 'notranslate'
-            },
-            title: 'Your No Label Developer',
-            meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
-                { name: 'theme-color', content: '#fefefe' },
-                { hid: 'fragment', name: 'fragment', content: '!' },
-            ],
-            link: [
-                { rel: 'icon', type: 'image/x-icon', href: 'https://dxlliv.github.io/favicon.ico' },
-                { rel: 'preload', type: 'font/woff2', href: '/fonts/minimal.woff2', as: 'font' },
-            ],
-        },
-        buildAssetsDir: 'assets/',
-        cdnURL: 'https://dxlliv.github.io',
-    },
-    css: [
-        "@fontsource/quicksand",
-        "@fontsource/quicksand/400.css",
-        "@fontsource/rock-salt",
-        "@fontsource/rock-salt/400.css",
-        "@fontsource/bebas-neue",
-        "@fontsource/bebas-neue/400.css",
-        '@mdi/font/css/materialdesignicons.css',
-    ],
-    components: [
-        {
-            path: '~/components',
-            pathPrefix: false,
-        },
-    ],
-    modules: [
-        '@nuxtjs/i18n',
-        '@pinia/nuxt',
-    ],
-    imports: {
-        dirs: ['stores','utils']
-    },
-    build: {
-        transpile: ['vuetify'],
-    },
-    i18n: {
-        customRoutes: 'config',
-        detectBrowserLanguage: {
-            useCookie: true
-        },
-    },
-    vue: {
-        compilerOptions: {
-            isCustomElement: (tag) => ['swiper-container', 'swiper-slide'].includes(tag),
-        },
-    },
-    nitro: {
-        output: {
-            publicDir: path.join(__dirname, 'docs')
-        }
-    },
+  ssr: true,
+
+  app: {
+      head: {
+          htmlAttrs: {
+              translate: 'no',
+              class: 'notranslate'
+          },
+          title: 'Your No Label Developer',
+          meta: [
+              { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+              { name: 'theme-color', content: '#fefefe' },
+              { hid: 'fragment', name: 'fragment', content: '!' },
+          ],
+          link: [
+              { rel: 'icon', type: 'image/x-icon', href: 'https://dxlliv.github.io/favicon.ico' },
+              { rel: 'preload', type: 'font/woff2', href: '/fonts/minimal.woff2', as: 'font' },
+          ],
+      },
+      buildAssetsDir: 'assets/',
+      cdnURL: 'https://dxlliv.github.io',
+  },
+
+  css: [
+      "@fontsource/quicksand",
+      "@fontsource/quicksand/400.css",
+      "@fontsource/rock-salt",
+      "@fontsource/rock-salt/400.css",
+      "@fontsource/bebas-neue",
+      "@fontsource/bebas-neue/400.css",
+      '@mdi/font/css/materialdesignicons.css',
+  ],
+
+  components: [
+      {
+          path: '~/components',
+          pathPrefix: false,
+      },
+  ],
+
+  modules: [
+      '@nuxtjs/i18n',
+      '@pinia/nuxt',
+  ],
+
+  imports: {
+      dirs: ['stores','utils']
+  },
+
+  build: {
+      transpile: ['vuetify'],
+  },
+
+  i18n: {
+      customRoutes: 'config',
+      detectBrowserLanguage: {
+          useCookie: true
+      },
+  },
+
+  vue: {
+      compilerOptions: {
+          isCustomElement: (tag) => ['swiper-container', 'swiper-slide'].includes(tag),
+      },
+  },
+
+  nitro: {
+      output: {
+          publicDir: path.join(__dirname, 'docs')
+      }
+  },
+
+  compatibilityDate: '2024-09-27',
 })
