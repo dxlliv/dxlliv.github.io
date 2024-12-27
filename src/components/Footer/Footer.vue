a2527efefa64c61a34cc41875d3b3f7ea9dfcc58
dxlliv <hello@dxlliv.dev>
2024-12-27 09:00:08 +0100

    Improve footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,69 +3,76 @@ import {GitHubIcon, InstagramIcon, XIcon, YouTubeIcon} from "vue3-simple-icons";
 </script>
 
 <template>
-  <div class="dx-toolbar">
-    <v-row>
-      <v-col :md="8">
+  <div class="dx-footer">
 
-        Turning visions into reality.<br />
+    <v-row>
+      <v-col :md="3" class="hidden-sm-and-down" align-self="center">
+
+        <!--
+        <FooterButton
+            href="https://x.com/dxlliv"
+        >
+          <XIcon :size="18"/>
+        </FooterButton>
+
+        <FooterButton
+            href="https://github.com/dxlliv"
+        >
+          <GitHubIcon :size="22"/>
+        </FooterButton>
+        -->
+
+      </v-col>
+      <v-col :md="6">
+
+        Turning visions into reality.<br/>
         Powered by the modern web.
         <div class="text-grey-lighten-1 font-weight-thin">dxlliv, formerly Diego Villa</div>
 
       </v-col>
       <v-col :md="3" class="hidden-sm-and-down" align-self="center">
 
-        <div class="mb-4">
-          <FooterButton
-              href="https://x.com/dxlliv"
-          >
-            <XIcon :size="18"/>
-          </FooterButton>
+        <!--
+        <FooterButton
+            href="https://instagram.com/dxlliv"
+        >
+          <InstagramIcon :size="18"/>
+        </FooterButton>
 
-          <FooterButton
-              href="https://github.com/dxlliv"
-          >
-            <GitHubIcon :size="22"/>
-          </FooterButton>
-        </div>
-
-        <div>
-
-          <FooterButton
-              href="https://instagram.com/dxlliv"
-          >
-            <InstagramIcon :size="18"/>
-          </FooterButton>
-
-          <FooterButton
-              href="https://youtube.com/@dxlliv"
-          >
-            <YouTubeIcon :size="22"/>
-          </FooterButton>
-        </div>
+        <FooterButton
+            href="https://youtube.com/@dxlliv"
+        >
+          <YouTubeIcon :size="22"/>
+        </FooterButton>
+        -->
 
       </v-col>
     </v-row>
+
   </div>
 </template>
 
 <style scoped lang="scss">
-.dx-toolbar {
+.dx-footer {
   display: grid;
   align-items: center;
   text-align: center;
-  height: 30%;
-  font-size: 22px;
+  height: 26dvh;
+
+  .v-btn {
+    opacity: 0.25;
+  }
 
   i {
     opacity: 0.8;
   }
 
-  @media(max-width: 1279px) {
-    font-size: 21px;
+  @media(min-width: 1280px) {
+    font-size: 22px;
   }
 
-  @media(max-width: 599px) {
-    font-size: 19px;
+  @media(min-width: 600px) {
+    font-size: 21px;
   }
 }
 </style>
