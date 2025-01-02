56f7f313983314580c38726726db611a50e11aee
dxlliv <hello@dxlliv.dev>
2025-01-03 00:26:57 +0100

    Update agent base url

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
     agent: {
-      baseURL: 'https://agent.dxlliv.dev',
+      baseURL: 'https://api.dxlliv.dev/1b47e87d-0278-0e58-a171-f3221a7e4c03',
     },
     links: {
         instagram: "https://instagram.com/dxlliv",
