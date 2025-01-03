a0597de371cdac2e36aac87f0a8a35ada2656c1e
dxlliv <hello@dxlliv.dev>
2025-01-03 09:33:34 +0100

    GenerateUUID can now generate different IDs in ChatManager.ts

--- app/models/ChatManager.ts
+++ app/models/ChatManager.ts
@@ -9,12 +9,12 @@ export class ChatManager {
         this.listElement = listElement
         this.agentBaseURL = agentBaseURL
 
-        this.userId = `user-${ChatManager.generateUUID()}`
-        this.roomId = `default-room-${ChatManager.generateUUID()}`
+        this.userId = ChatManager.generateID('xxxxxxx')
+        this.roomId = `room-${ChatManager.generateID('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx')}`
     }
 
-    static generateUUID(): string {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    static generateID(template: string): string {
+        return template.replace(/[xy]/g, (c) => {
             const r = Math.random() * 16 | 0
             const v = c === 'x' ? r : (r & 0x3 | 0x8)
             return v.toString(16)
