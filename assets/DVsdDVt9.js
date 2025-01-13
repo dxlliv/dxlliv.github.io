295716d8d8a4901855f9c3c796e8936b50602e1d
dxlliv <hello@dxlliv.dev>
2025-01-13 10:14:51 +0100

    Update gh-pages

--- assets/DVsdDVt9.js
+++
@@ -1,1 +0,0 @@
-import{aq as s}from"./NWhJDZ4X.js";import{u}from"./CQ5Id23j.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
