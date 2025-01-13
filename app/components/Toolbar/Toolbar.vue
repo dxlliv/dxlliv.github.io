e0bb897f24bc6ccdb9bc7a64cfb61615e5964e89
dxlliv <hello@dxlliv.dev>
2025-01-13 10:11:17 +0100

    Add toolbar menu

--- app/components/Toolbar/Toolbar.vue
+++ app/components/Toolbar/Toolbar.vue
@@ -10,39 +10,20 @@ const sliderHorizontalStore = useSliderHorizontalStore()
     <v-divider/>
 
     <v-row no-gutters>
-      <v-col class="flex-grow-0 hidden-xs" align-self="center">
+      <v-col class="flex-grow-0" align-self="center">
 
         <v-breadcrumbs class="px-5 py-0">
-          <v-breadcrumbs-item to="/">
+          <v-breadcrumbs-item>
 
-            <AppLogo name="toolbar"/>
+            <AppLogo name="toolbar" class="cursor-pointer" />
+
+            <ToolbarMenu />
 
           </v-breadcrumbs-item>
         </v-breadcrumbs>
 
       </v-col>
-      <v-col class="px-3 flex-grow-0 hidden-sm-and-down" align-self="center">
-
-        <nuxt-link to="/experience" class="text-overline">
-          Design
-        </nuxt-link>
-
-      </v-col>
-      <v-col class="px-3 flex-grow-0 hidden-sm-and-down" align-self="center">
-
-        <nuxt-link to="/drones" class="text-overline">
-          Flying
-        </nuxt-link>
-
-      </v-col>
-      <v-col class="px-3 flex-grow-0 hidden-md-and-down" align-self="center">
-
-        <nuxt-link to="/archive" class="text-overline">
-          Archive
-        </nuxt-link>
-
-      </v-col>
-      <v-col class="flex-grow-1 ml-3 px-0 text-right overflow-hidden" align-self="center">
+      <v-col class="flex-grow-1 px-0 text-right overflow-hidden" align-self="center">
 
         <ToolbarMarquee
           @click="sliderHorizontalStore.slideTo(0)"
@@ -55,14 +36,14 @@ const sliderHorizontalStore = useSliderHorizontalStore()
           <v-breadcrumbs-item>
 
             <a href="https://github.com/sponsors/dxlliv" target="_blank">
-              <GitHubIcon :size="22" style="margin-top: -1px;" />
+              <GitHubIcon :size="23" />
             </a>
 
           </v-breadcrumbs-item>
           <v-breadcrumbs-item class="ml-3">
 
             <a href="https://instagram.com/dxlliv" target="_blank">
-              <InstagramIcon :size="21"/>
+              <InstagramIcon :size="22" />
             </a>
 
           </v-breadcrumbs-item>
@@ -86,7 +67,6 @@ const sliderHorizontalStore = useSliderHorizontalStore()
 .dx-toolbar {
   height: var(--dx-toolbar-height);
   line-height: calc(var(--dx-toolbar-height) - 2px) !important;
-  opacity: 0.8;
   overflow: hidden;
 
   @media(max-width: 479px) {
@@ -100,7 +80,12 @@ const sliderHorizontalStore = useSliderHorizontalStore()
   padding: 0;
 }
 
+.dx-logo {
+  margin-top: -2px;
+}
+
 svg {
-  vertical-align: middle
+  vertical-align: middle;
+  margin-top: -3px;
 }
 </style>
---
+++ app/components/Toolbar/ToolbarMenu.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+const buttonProps = {
+  flat: true,
+  active: false,
+  icon: true,
+  width: 64,
+  height: 64,
+}
+
+const menu = ref(false)
+
+function onMenuClose() {
+  menu.value = false
+}
+</script>
+
+<template>
+  <v-menu
+      activator="parent"
+      eager
+      v-model="menu"
+      :offset="[9, 12]"
+  >
+    <v-card :width="320" max-width="100vw" rounded>
+      <v-card-text class="px-7">
+
+        <ToolbarMenuTitle title="Design & Web Development"/>
+
+        <ToolbarMenuItems class="mb-4">
+          <v-breadcrumbs-item>
+            <v-btn
+                v-bind="buttonProps"
+                to="/"
+            >
+              <Icon
+                  name="lucide:home" :size="27"
+                  class="text-center mx-2"
+              />
+            </v-btn>
+          </v-breadcrumbs-item>
+          <v-breadcrumbs-item>
+            <v-btn
+                v-bind="buttonProps"
+                to="/experience"
+            >
+              <Icon
+                  name="lucide:code-xml" :size="28"
+                  class="text-center mx-2"
+              />
+            </v-btn>
+          </v-breadcrumbs-item>
+          <v-breadcrumbs-item>
+            <v-btn
+                v-bind="buttonProps"
+                href="mailto:hello@dxlliv.dev"
+                @click="onMenuClose"
+            >
+              <Icon
+                  name="mdi:at" :size="26"
+                  class="text-center mx-2"
+              />
+
+              <ContactDialog/>
+            </v-btn>
+          </v-breadcrumbs-item>
+        </ToolbarMenuItems>
+
+        <ToolbarMenuTitle title="Drones & Commissions"/>
+
+        <ToolbarMenuItems class="mb-2">
+          <v-breadcrumbs-item>
+            <v-btn
+                v-bind="buttonProps"
+                @click="onMenuClose"
+            >
+              <Icon
+                  name="lucide:image-play" :size="28"
+                  class="text-center mx-2"
+              />
+
+              <ShootingDialog/>
+            </v-btn>
+          </v-breadcrumbs-item>
+          <v-breadcrumbs-item>
+            <v-btn
+                v-bind="buttonProps"
+                to="/drones"
+            >
+              <Icon
+                  name="tabler:drone" :size="29"
+                  style="margin-top: -1px;"
+                  class="text-center mx-2"
+              />
+            </v-btn>
+          </v-breadcrumbs-item>
+          <v-breadcrumbs-item>
+            <v-btn
+                v-bind="buttonProps"
+                @click="onMenuClose"
+            >
+              <Icon
+                  name="lucide:calendar-days" :size="28"
+                  class="text-center mx-2"
+                  style="margin-top: -4px;"
+              />
+
+              <AgentDialog/>
+            </v-btn>
+          </v-breadcrumbs-item>
+        </ToolbarMenuItems>
+
+      </v-card-text>
+    </v-card>
+  </v-menu>
+</template>
