39cee4cb7bebc6ecbda83dc90735afad71ac97ad
dxlliv <hello@dxlliv.dev>
2025-01-16 15:18:01 +0100

    Update gh-pages

---
+++ assets/Pa9b4iWA.js
@@ -0,0 +1,1 @@
+import{av as s}from"./bhUIG5zN.js";import{u}from"./Dfvr58Bn.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
