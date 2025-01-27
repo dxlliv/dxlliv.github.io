6fd2039f423ec0b3a0bde0516dd6c948d408a672
dxlliv <hello@dxlliv.dev>
2025-01-27 21:08:42 +0100

    Refactor agent components

---
+++ app/components/Agent/AgentLauncher.vue
@@ -0,0 +1,5 @@
+<template>
+  <AppDialog>
+    <AgentForm />
+  </AppDialog>
+</template>
---
+++ app/components/App/AppDialog.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  fullscreen?: boolean
+}>()
+</script>
+
+<template>
+  <v-bottom-sheet
+      v-if="$vuetify.display.xs"
+      activator="parent"
+  >
+    <v-card class="v-card--rounded-top">
+      <slot />
+    </v-card>
+  </v-bottom-sheet>
+  <v-dialog
+      v-else
+      :width="600"
+      max-width="calc(100vw - 36px)"
+      :fullscreen="fullscreen"
+      transition="dialog-bottom-transition"
+      activator="parent"
+  >
+    <v-card :rounded="!fullscreen">
+      <slot />
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped lang="scss">
+
+</style>
