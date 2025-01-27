36eba18844aa7bbb2c11ac4a0a1a14b3973b658a
dxlliv <hello@dxlliv.dev>
2025-01-27 21:26:23 +0100

    Disable zoom from app viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,8 @@ export default defineNuxtConfig({
 
     app: {
         head: {
-            title: 'Your No Label Developer',
             meta: [
-                {name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'},
+                { name: 'viewport', content: 'viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
                 {name: 'mobile-web-app-capable', content: 'yes'},
                 {name: 'mobile-web-app-status-bar-style', content: 'black-translucent'},
                 {name: 'theme-color', content: '#000'},
