e3e5993b030f8d2b34c025bf794103c87d99286b
dxlliv <hello@dxlliv.dev>
2025-01-29 14:08:16 +0100

    Update design image in home screen

--- app/components/Home/HomeButtons.vue
+++ app/components/Home/HomeButtons.vue
@@ -24,7 +24,7 @@ function getDailyImage(items) {
 
       <HomeButton
           :title="$t('home.button.design')" marquee
-          image="https://images.unsplash.com/photo-1503149779833-1de50ebe5f8a?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image="/media/home/design.jpg"
           to="/design"
       />
 
