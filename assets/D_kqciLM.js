983375af67d402719da40d1b94c8704ebf004c3f
dxlliv <hello@dxlliv.dev>
2025-04-12 16:22:41 +0200

    Update gh-pages

--- assets/D_kqciLM.js
+++
@@ -1,1 +0,0 @@
-import{u,as as l}from"./Cx0vDjmN.js";function f(e,a){const{title:r,titleTemplate:s,...n}=e;return u({title:r,titleTemplate:s,_flatMeta:n},{...a,transform(t){const o=l({...t._flatMeta});return delete t._flatMeta,{...t,meta:o}}})}export{f as u};
